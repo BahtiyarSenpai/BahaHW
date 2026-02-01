0b6f3a5e9055d6b930ff7418b064381b8bdb4b3b
FT-200 <danyckmister@gmail.com>
2026-02-01 18:01:01 +0500

    at hopefull least it's not a 67th task

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,11 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        string s = Console.ReadLine();
+        if (s.StartsWith("а"))
+        {
+            Console.WriteLine("Начинается на А");
+        }
     }
 }
 //Why are you Baha
