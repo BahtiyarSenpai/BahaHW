89e667fcc716c4ce42140b1d9f19922e9ea93793
Student <sunshbaha@gmail.com>
2026-02-01 17:37:09 +0500

    18(18+)

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,6 +4,9 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        for (int i = 5; i >= 1; i--)
+        {
+            Console.Write(i + " ");
+        }
     }
 }
