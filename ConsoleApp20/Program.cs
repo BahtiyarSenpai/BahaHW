95861b44dadc396ddaf35fee38ef3994f9ef9ca5
Student <sunshbaha@gmail.com>
2026-02-01 17:19:08 +0500

    6

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,6 +4,15 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        if (a > b)
+        {
+            Console.WriteLine(a);
+        }
+        else
+        {
+            Console.WriteLine(b);
+        }
     }
 }
