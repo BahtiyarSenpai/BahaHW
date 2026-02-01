9e64bb09466db395a76aa108d56e061e4663025c
FT-200 <danyckmister@gmail.com>
2026-02-01 17:57:38 +0500

    Windy-31

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,10 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        for (int i = 1; i <= 4; i++)
+        {
+            Console.Write(i * i + " ");
+        }
     }
 }
 //Why are you Baha
