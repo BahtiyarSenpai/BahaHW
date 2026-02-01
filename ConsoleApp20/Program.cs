b99d53957cf7ff299c9d9a2f2c8c634c2f591162
Student <sunshbaha@gmail.com>
2026-02-01 17:49:50 +0500

    u brain is empty

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,6 +4,14 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        string s = Console.ReadLine();
+        if (s == "")
+        {
+            Console.WriteLine("Пусто");
+        }
+        else
+        {
+            Console.WriteLine("Текст есть");
+        }
     }
 }
