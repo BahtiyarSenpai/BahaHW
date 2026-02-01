4a1ec801516c863c44f8d00b1aae891f8dc79d46
FT-200 <danyckmister@gmail.com>
2026-02-01 17:43:05 +0500

    Baha = Else;

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,19 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int mark = int.Parse(Console.ReadLine());
+        if (mark == 5)
+        {
+            Console.WriteLine("Отличник!");
+        }
+        else if (mark == 4)
+        {
+            Console.WriteLine("Хорошая Оценка");
+        }
+        else
+        {
+            Console.WriteLine("Деградация?");
+        }
     }
 }
 //Why are you Baha
