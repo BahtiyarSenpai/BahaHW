889d6170f2450aa3f6eb52c4c277ae346a5e70bc
Student <sunshbaha@gmail.com>
2026-02-01 17:21:20 +0500

    8(Bahabest)

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,6 +4,14 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        string s = Console.ReadLine();
+        if (s == "Bahabest")
+        {
+            Console.WriteLine("Верно");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка");
+        }
     }
 }
