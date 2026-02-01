b6a1b19b0ed4697b71895c56f3453a6a57c21297
FT-200 <danyckmister@gmail.com>
2026-02-01 17:58:44 +0500

    IDK what should I say?

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,8 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        string s = Console.ReadLine();
+        Console.WriteLine($"{s}{s}");
     }
 }
 //Why are you Baha
