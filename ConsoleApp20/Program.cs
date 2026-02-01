dbe8f6f5883fb6a510d3954f0b3146d7ae3476dd
FT-200 <danyckmister@gmail.com>
2026-02-01 17:52:02 +0500

    I am the Great Papyrus!

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,11 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        string s = Console.ReadLine();
+        if (s.Contains("a"))
+        {
+            Console.WriteLine("Нашлось");
+        }
     }
 }
 //Why are you Baha
