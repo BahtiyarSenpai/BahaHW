d45590a188d429bde0b3377cae1d74b66748c799
Student <sunshbaha@gmail.com>
2026-02-01 17:54:08 +0500

    Astana weather

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,6 +4,18 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int t = int.Parse(Console.ReadLine());
+        if (t < 0)
+        {
+            Console.WriteLine("Мороз");
+        }
+        else if (t > 25)
+        {
+            Console.WriteLine("Жара");
+        }
+        else
+        {
+            Console.WriteLine("Нормально");
+        }
     }
 }
