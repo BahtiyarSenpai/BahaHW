02d737d5721b25fd038def7365256d6b9c3912d3
Empty Watcher <danyckmister@gmail.com>
2026-02-01 16:40:29 +0500

    fix mergeAdd comment in Program.cs
    Added a comment for clarification.

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,6 +4,13 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        string word = Console.ReadLine();
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(word + " ");
+        }
     }
 }
+
+//fixing the merge 1
