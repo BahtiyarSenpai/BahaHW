c00895fd106e5ef67d782a6407a7404e5a572d3b
Student <sunshbaha@gmail.com>
2026-02-01 17:08:42 +0500

    4

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,6 +4,14 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int age = int.Parse(Console.ReadLine());
+        if (age >= 18)
+        {
+            Console.WriteLine("Сигма");
+        }
+        else
+        {
+            Console.WriteLine("Маловат еще,иди поплачь,мелочь");
+        }
     }
 }
