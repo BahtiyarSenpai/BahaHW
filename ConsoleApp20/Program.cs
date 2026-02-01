d95dabe8a6b510680a92204b3e1f379b89b64e74
FT-200 <danyckmister@gmail.com>
2026-02-01 17:40:30 +0500

    STFU

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,10 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write(i * 2 + " ");
+        }
     }
 }
 //Why are you Baha
