dee9ef56a90cd0b75511cc158a402c73ff65add4
FT-200 <danyckmister@gmail.com>
2026-02-01 17:46:36 +0500

    What a Beatiful day outside. Birds are singing, Bahas are Blooming. On days like this, Bahas like you... Should be Burning in Purgatory, and Hell's Lust Circle

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,15 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        string day = Console.ReadLine();
+        if (day == "суббота" || day == "воскресенье")
+        {
+            Console.WriteLine("Выходной");
+        }
+        else
+        {
+            Console.WriteLine("Работа");
+        }
     }
 }
 //Why are you Baha
