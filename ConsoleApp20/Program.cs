6fc2ab6a1343af66ad477fe2a28c898cb2d8ca0d
FT-200 <danyckmister@gmail.com>
2026-02-01 17:33:54 +0500

    nein

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,12 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int i = 1;
+        while (i <= 5)
+        {
+            Console.Write(i + " ");
+            i++;
+        }
     }
 }
 //Why are you Baha
