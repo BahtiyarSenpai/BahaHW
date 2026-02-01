26a4c0377c8499d11222a3db2fc42cf628752030
FT-200 <danyckmister@gmail.com>
2026-02-01 17:55:55 +0500

    Why does it take so long!?!?

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,9 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        Console.WriteLine((a + b) / 2);
     }
 }
 //Why are you Baha
