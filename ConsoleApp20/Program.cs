108cbb61106895f8bd54509a571730f14373b412
FT-200 <danyckmister@gmail.com>
2026-02-01 17:38:38 +0500

    Bahu Voorheez, will find you

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,14 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+        while (n < 100)
+        {
+            n = n * 2;
+        }
+        Console.WriteLine(n);
     }
 }
-//Why are you Baha
+/*Why are you Baha?
+
+Because it's Friday 13th! *?
