65fd5fa8c275a9803a0c372435a6e5f96cce5135
FT-200 <danyckmister@gmail.com>
2026-02-01 17:59:48 +0500

    it was utterly unnerving

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,11 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+        if (n % 2 == 0 && n % 5 == 0)
+        {
+            Console.WriteLine("Подходит");
+        }
     }
 }
 //Why are you Baha
