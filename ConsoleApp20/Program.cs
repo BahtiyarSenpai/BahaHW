e88c8e261615199255712a74bf10b3a40b045a47
FT-200 <danyckmister@gmail.com>
2026-02-01 17:54:35 +0500

    Baha = yag

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,12 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        if (a > 0 && b > 0)
+        {
+            Console.WriteLine(a * b);
+        }
     }
 }
 //Why are you Baha
