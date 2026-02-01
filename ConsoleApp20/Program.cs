3d7dd7f5d20e1380cce4c298d216e8d618855ccd
FT-200 <danyckmister@gmail.com>
2026-02-01 18:04:25 +0500

    Finnish Finish

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,16 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        if (a > b)
+        {
+            Console.WriteLine(a - b);
+        }
+        else
+        {
+            Console.WriteLine(b - a);
+        }
     }
 }
 //Why are you Baha
