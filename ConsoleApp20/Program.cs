4e7e716913563075f53cb154aa2aa8cd08e28de9
FT-200 <danyckmister@gmail.com>
2026-02-01 17:48:45 +0500

    Какие у тебя суммы?!

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,13 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum = sum + i;
+        }
+        Console.WriteLine(sum);
     }
 }
 //Why are you Baha
