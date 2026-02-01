8d0af2aa8c7851358c9904fb3aec90d0feeff269
Student <sunshbaha@gmail.com>
2026-02-01 17:39:23 +0500

    bro i dont know what write here

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,6 +4,10 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+        if (n % 3 == 0)
+        {
+            Console.WriteLine("Да");
+        }
     }
 }
