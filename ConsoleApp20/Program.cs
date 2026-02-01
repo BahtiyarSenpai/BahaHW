a930e0689432eeba76edbdbf3d7946668eaa734f
FT-200 <danyckmister@gmail.com>
2026-02-01 17:53:11 +0500

    5 * 5

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,8 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Результат: {n * 10}");
     }
 }
 //Why are you Baha
