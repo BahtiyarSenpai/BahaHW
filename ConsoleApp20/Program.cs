96f196cafb1c0f6701bb8eb4a95a45ceb1b5180e
Student <sunshbaha@gmail.com>
2026-02-01 17:51:43 +0500

    for the emperor

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,6 +4,11 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+        while (n >= 0)
+        {
+            Console.Write(n + " ");
+            n--;
+        }
     }
 }
