669b1fadf1a4bd649aa8f341c573727cfa488c98
Student <sunshbaha@gmail.com>
2026-02-01 17:32:35 +0500

    14

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,6 +4,10 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+        if (n == 0)
+        {
+            Console.WriteLine("Это ноль");
+        }
     }
 }
