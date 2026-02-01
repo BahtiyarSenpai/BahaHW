b45ba16835a62388447885bfc554a9fc348f0f8c
Student <sunshbaha@gmail.com>
2026-02-01 18:03:23 +0500

    67 67 6767 676767

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,6 +4,14 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+        if (n == 7)
+        {
+            Console.WriteLine("Счастливое число!");
+        }
+        else
+        {
+            Console.WriteLine("Обычное число");
+        }
     }
 }
