be313da2b61f18beedde544fb6304ca297ffc6ca
FT-200 <danyckmister@gmail.com>
2026-02-01 17:36:27 +0500

    11

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,7 +4,15 @@ public class BahaSenpai
 {
     public static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+        if (n >= 10 && n <= 20)
+        {
+            Console.WriteLine("В диапазоне");
+        }
+        else
+        {
+            Console.WriteLine("Не в Диапозоне");
+        }
     }
 }
 //Why are you Baha
